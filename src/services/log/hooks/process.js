156d3f31e531101dfac0fe01b41057757c5dcba0
Bharat Arimilli <bbarimilli@outlook.com>
2018-04-11 23:19:05 -0500

    Don't need to request target user, this is automatically included in the request from the gateway

--- src/services/log/hooks/process.js
+++ src/services/log/hooks/process.js
@@ -3,7 +3,6 @@ const data = require('../../../utils/data');
 
 module.exports = function () {
   return async function (hook) {
-    hook.data.targetUser = await data.getUser(hook.params.authorization, hook.data.targetUserId);
     hook.data.organizationId = hook.params.user.currentOrganizationId;
 
     return hook;
