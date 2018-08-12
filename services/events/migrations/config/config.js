5f21906a7bca777a6f4233530c2451f494d9e0d1
Bharat Arimilli <bbarimilli@outlook.com>
2018-08-12 07:39:42 -0500

    Update events sequelize config for migrations

--- services/events/migrations/config/config.js
+++ services/events/migrations/config/config.js
@@ -1,4 +1,9 @@
+if (!process.env.DATABASE_URL) {
+  require('dotenv').config();
+}
+
 const env = process.env.NODE_ENV || 'development';
+const ssl = process.env.DATABASE_SSL === 'true' ? true : false;
 
 module.exports = {
   [env]: {
@@ -6,7 +11,7 @@ module.exports = {
     dialect: 'postgres',
     migrationStorageTableName: '_migrations',
     dialectOptions: {
-      ssl: (env === 'development') ? false : true,
+      ssl,
     },
   }
 };
